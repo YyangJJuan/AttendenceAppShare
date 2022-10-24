aa5d95c1281b0e4a7429b3f2f7fc17a225f98124
YyangJJuan <1343296479@qq.com>
2022-10-24 23:49:53 +0800

    2022-10-24 23:49

--- app/src/main/java/com/example/application2/MainActivity.java
+++ app/src/main/java/com/example/application2/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.application2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void GoSecond(View v)
+    {
+        //intent设置要跳转的页面
+        Intent intent = new Intent(this,ForgetPwd.class);
+        //进行跳转
+        startActivity(intent);
     }
 }
